26d8b4626e08fbaf3b3ab57a045db48920680abb
Benny Degezelle <benny@gorilla-webdesign.be>
2009-02-17 02:25:56 +0100

    allowing site_language to run it's migrations

--- tags_extension.rb
+++ tags_extension.rb
@@ -12,17 +12,19 @@ class TagsExtension < Radiant::Extension
     if Radiant::Config['tags.results_page_url'].blank?
       Radiant::Config['tags.results_page_url'] = TagsExtension::DEFAULT_RESULTS_URL if Radiant::Config['tags.results_page_url'].blank?
     end
-    if defined?(SiteLanguage) && SiteLanguage.count > 0
-      include Globalize
-      SiteLanguage.codes.each do |code|
-        langname = Locale.new(code).language.code
-        map.connect "#{langname}#{Radiant::Config['tags.results_page_url']}/:tag", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url'], :language => code
+    begin
+      if defined?(SiteLanguage) && SiteLanguage.count > 0
+        include Globalize
+        SiteLanguage.codes.each do |code|
+          langname = Locale.new(code).language.code
+          map.connect "#{langname}#{Radiant::Config['tags.results_page_url']}/:tag", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url'], :language => code
+        end
+      else
+        map.connect "#{Radiant::Config['tags.results_page_url']}/:tag", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url']
       end
     rescue
       # dirty hack; need to get trough here to allow migrations to run..
-    else
-      map.connect "#{Radiant::Config['tags.results_page_url']}/:tag", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url']
-    end
+    end  
   end
   
   def activate
