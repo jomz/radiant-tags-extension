4616cf9817591ce5dc6692ce5524288adfd3ae3c
Edmund Haselwanter <edmund@haselwanter.com>
2010-12-23 23:53:18 +0100

    move routes to routes.rb

--- tags_extension.rb
+++ tags_extension.rb
@@ -8,27 +8,6 @@ class TagsExtension < Radiant::Extension
   
   DEFAULT_RESULTS_URL = '/search/by-tag'
 
-  define_routes do |map|
-    if Radiant::Config['tags.results_page_url'].blank?
-      Radiant::Config['tags.results_page_url'] = TagsExtension::DEFAULT_RESULTS_URL if Radiant::Config['tags.results_page_url'].blank?
-    end
-    begin
-      if defined?(SiteLanguage) && SiteLanguage.count > 0
-        include Globalize
-        SiteLanguage.codes.each do |code|
-          langname = Locale.new(code).language.code
-          map.connect "#{langname}#{Radiant::Config['tags.results_page_url']}/:tag", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url'], :language => code
-        end
-      else if defined?(VhostExtension)
-        map.connect "#{Radiant::Config['tags.results_page_url']}/:tag", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url']
-      end
-        map.connect "#{Radiant::Config['tags.results_page_url']}/:tag", :controller => 'site', :action => 'show_page', :url => Radiant::Config['tags.results_page_url']
-      end
-    rescue
-      # dirty hack; need to get trough here to allow migrations to run..
-    end  
-  end
-  
   def activate
     raise "The Shards extension is required and must be loaded first!" unless defined?(admin.page)
     if Radiant::Config.table_exists?
